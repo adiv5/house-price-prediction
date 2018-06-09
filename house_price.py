51973bf94e3f75016505e2067e797fc5b10ed404
Aditya Vartak <aadivartak@gmail.com>
2018-06-09 20:39:52 +0530

    Add files via upload

--- house_price.py
+++ house_price.py
@@ -3,6 +3,9 @@ Created on Fri May 25 10:23:43 2018
 
 @author: Aditya Vartak
 
+Topic: Housing Price prediction in Ames
+
+Referred kernel: Stacked Regressions by Serigne
 """
 import numpy as np
 import pandas as pd 
@@ -83,7 +86,6 @@ plt.show()
 
 #we see that the Normalised  graph is shifted to left so it is right skewed
 #hence we do log transformation of targe varaible
-
 train["SalePrice"] = np.log1p(train["SalePrice"])
 #check new Normal Distrubution
 sns.distplot(train['SalePrice'] , fit=norm);
@@ -276,7 +278,7 @@ print(all_data.shape)
 train = all_data[:ntrain]
 test = all_data[ntrain:]
 
-#
+
 #MODELLING
 from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
 from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
@@ -351,59 +353,9 @@ class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
 averaged_models = AveragingModels(models = (ENet, Boost, KRR, lasso))
 
 score = rmsle_cv(averaged_models)
-class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
-    def __init__(self, base_models, meta_model, n_folds=5):
-        self.base_models = base_models
-        self.meta_model = meta_model
-        self.n_folds = n_folds
-   
-    # We again fit the data on clones of the original models
-    def fit(self, X, y):
-        self.base_models_ = [list() for x in self.base_models]
-        self.meta_model_ = clone(self.meta_model)
-        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
-        
-        # Train cloned base models then create out-of-fold predictions
-        # that are needed to train the cloned meta-model
-        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
-        for i, model in enumerate(self.base_models):
-            for train_index, holdout_index in kfold.split(X, y):
-                instance = clone(model)
-                self.base_models_[i].append(instance)
-                instance.fit(X[train_index], y[train_index])
-                y_pred = instance.predict(X[holdout_index])
-                out_of_fold_predictions[holdout_index, i] = y_pred
-                
-        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
-        self.meta_model_.fit(out_of_fold_predictions, y)
-        return self
-   
-    #Do the predictions of all base models on the test data and use the averaged predictions as 
-    #meta-features for the final prediction which is done by the meta-model
-    def predict(self, X):
-        meta_features = np.column_stack([
-            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
-            for base_models in self.base_models_ ])
-        return self.meta_model_.predict(meta_features)
-    stacked_averaged_models = StackingAveragedModels(base_models = (ENet, Boost, KRR),
-                                                 meta_model = lasso)
-
-score = rmsle_cv(stacked_averaged_models)
-print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
-
-def rmsle(y, y_pred):
-    return np.sqrt(mean_squared_error(y, y_pred))
-
-#Ensemble stackedRegressor
-stacked_averaged_models.fit(train.values, y_train)
-stacked_train_pred = stacked_averaged_models.predict(train.values)
-stacked_pred = np.expm1(stacked_averaged_models.predict(test.values))
-print(rmsle(y_train, stacked_train_pred))
-
-
-sub = pd.DataFrame()
-sub['Id'] = test_ID
-sub['SalePrice'] = stacked_pred
-sub.to_csv('submission.csv',index=False)
-
- 
+print(" Averaged base models score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+AveragingModels.fit(train.values, y_train)
+predicted=AveragingModels.predict(train.values)    
+print(predicted)
+sub=pd.dataframe()
+sub['íd']=test_ID
